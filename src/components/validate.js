1b6351389b83b9e7a427e2b23f0f596b74b9a1eb
SofiaPurtova <sofiapurtova@yandex.ru>
2024-12-23 18:02:55 +0300

    second attempt

--- src/components/validate.js
+++ src/components/validate.js
@@ -54,8 +54,7 @@ export const enableValidation = (cfg) => {
         const fieldsetList = Array.from(formElement.querySelectorAll('.form__set'));
         fieldsetList.forEach((fieldSet) => {
             setEventListeners(fieldSet, cfg);
-        });
-      //setEventListeners(formElement);
+        });s
     });
 }
 
